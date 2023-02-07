e1c83713369f84af2c96da39ea281dd4723888dc
veerendra363 <veeruparadesi363@gmail.com>
2023-02-07 10:21:08 +0530

    sum of n natural numbers using recursion problem and solution added

---
+++ Recursion/Problems/Sum_of_N_natural_numbers/problem_soltuion.py
@@ -0,0 +1,25 @@
+# https://www.youtube.com/watch?v=69ZCDFy-OUo&list=PLgUwDviBIf0rGlzIn_7rsaR2FQ5e6ZOL9
+# Sum of n natural numbers using recursion
+# Example:
+# n = 10 
+# 55
+
+#parameterized 
+def sumOfNums(n, sum):
+    if ( n==0 ):
+        return sum
+    return sumOfNums(n-1, n+sum)
+
+n = int(input('Enter number : '))
+s = sumOfNums(n, 0)
+print(f'Sum of {n} numbers = {s}')
+
+#functional recursion
+def sumOfNums(n):
+    if ( n==1 ):
+        return 1
+    return n + sumOfNums(n-1)
+
+n = int(input('Enter number : '))
+s = sumOfNums(n)
+print(f'Sum of {n} numbers = {s}')
---
+++ Recursion/recursion.md
@@ -0,0 +1,17 @@
+# Recursion
+A function is calling itself until a specific condition(Base Case) is met.  
+fun(){  
+    condition(Base Case){  
+        return val  
+    }   
+    fun()   
+}  
+### Key Words
+-> Stack Space
+-> Base case
+-> Recursion Tree
+
+
+
+
+
